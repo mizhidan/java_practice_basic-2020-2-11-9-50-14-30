5293ed9dbac1345d930443850a0aedc24ad7e507
mizhidan <dj9824@126.com>
2020-02-23 15:21:20 +0800

    [danjie] fixed: now it can print correct infos

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,7 +1,11 @@
 package com.thoughtworks;
 
+import java.text.ParseException;
+
 public class App {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ParseException {
+    Student student = new Student("张三","001","2019.02.11");
+    System.out.println(student);
   }
 }
--- src/main/java/com/thoughtworks/Student.java
+++ src/main/java/com/thoughtworks/Student.java
@@ -1,17 +1,19 @@
 package com.thoughtworks;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Student {
     private String name;
     private String id;
-    private SimpleDateFormat enrollDate;
+    private String enrollDate;
 
     public Student(String name,String id,String enrollDate) {
         this.name = name;
         this.id = id;
-        this.enrollDate = new SimpleDateFormat(enrollDate);
+        this.enrollDate = enrollDate;
     }
 
 
@@ -31,15 +33,37 @@ public class Student {
         this.id = id;
     }
 
-    public SimpleDateFormat getEnrollDate() {
-        return enrollDate;
+    public String getEnrollDate() throws ParseException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+        Date enrollDate = dateFormat.parse(this.enrollDate);
+        SimpleDateFormat newDateFormat = new SimpleDateFormat("yyyy年MM月dd日");
+        return newDateFormat.format(enrollDate);
     }
 
     public void setEnrollDate(String enrollDate) {
-        this.enrollDate = new SimpleDateFormat(enrollDate);
+        this.enrollDate = enrollDate;
+    }
+
+    public int getInSchoolYear() {
+        Date currentTime = new Date();
+        String enrollSchoolYear = enrollDate.split("\\.")[0];
+        DateFormat currentDate = new SimpleDateFormat("yyyy");
+        int inSchoolYear = Integer.parseInt(currentDate.format(currentTime)) - Integer.parseInt(enrollSchoolYear);
+        return inSchoolYear;
     }
 
     public Boolean isEquals(Student o) {
         return(this.id.equals(o.id));
+    }
+
+    @Override
+    public String toString() {
+
+        try {
+            return "我叫" + name + ",我的学号是" + id + "," +getEnrollDate() + "入学，学龄" + getInSchoolYear() + "年";
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return "abc";
     }
 }
